9d3fe4db9c3724a81b6aade011905924a9b63644
Alex <alexandraeaseeva@yandex.ru>
2020-03-25 14:30:31 +0300

    "Исходно заданные параметры"

---
+++ One proect/One proect/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_proect
+{
+    class Program
+    {
+        static void Main () 
+        {
+            int[] mas = new int[16];//Задаем массив на 16 элементов 
+          
+
+            for (int i = 0; i < mas.Length; i++)//Объявляем цикл для обработки массива
+            {
+                mas[i] = 0;//Заполняем массив 0
+            }
+
+                foreach (int i in mas)//Распечатываем значение элементов массива
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+                
+    }
+}
